6e22b4366759fbaa3913cebcb7b1fe97367729bb
Ta-Fuku <74134260+Ta-Fuku@users.noreply.github.com>
2021-01-01 04:08:44 +0900

    最初、Center

---
+++ testing.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//main は開始地点
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          'Hello World!',
+        ),
+      ),
+    ),
+  );
+}
+//真ん中にhelloworld
